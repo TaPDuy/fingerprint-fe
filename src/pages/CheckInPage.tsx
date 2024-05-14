3c13412010c9dc4f40519fa20b5e3330c2b80b86
duy.taphuong <duy.taphuong@trudi.ai>
2024-05-14 09:55:13 +0700

    Style checkin

--- src/pages/CheckInPage.tsx
+++ src/pages/CheckInPage.tsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { checkin } from "../services";
 import { useAPI } from "../utils";
 
 function CheckInPage() {
 
 	const [file, setFile] = useState<File>();
+	const [preview, setPreview] = useState<string>();
 	const { trigger: doCheckIn, isLoading, error } = useAPI(checkin);
+	const fileChooser = useRef<HTMLInputElement>(null);
 
 	const handleCheckin = () => {
 		if (!file) return;
@@ -16,18 +18,46 @@ function CheckInPage() {
 	}
 
 	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setFile(() => event.target.files?.[0]);
+		const selectedFile = event.target.files?.[0];
+		if (!selectedFile) {
+			setPreview(undefined);
+		} else {
+			const objUrl = URL.createObjectURL(selectedFile);
+			setPreview(() => objUrl);
+		}
+		setFile(() => selectedFile);
 	}
 
 	return (
 		<>
-			<h1>Check-in</h1>
 			{ error }
-			<div>
-				<input type="file" onChange={handleFileChange}/>
-				<button onClick={handleCheckin} disabled={!file || isLoading}>
-					{ isLoading ? 'Checking in...' : 'Check in' }
-				</button>
+			<div className="w-full flex items-center justify-center flex-col mt-16 gap-12">
+				{ file 
+					? <div className="flex flex-col gap-4 items-center">
+						<img className="w-[256px]" src={preview} alt="preview"/>
+						<h2>{ file.name }</h2>
+					</div>
+					: <div 
+						className="w-[256px] h-[256px] flex items-center justify-center bg-slate-300 text-slate-500"
+					>
+						<p>Image preview</p>
+					</div>
+				}
+				<div className="w-[256px] flex justify-between">
+					<label htmlFor="image-select">
+						<button 
+							className="bg-black text-white p-2 px-4"
+							onClick={() => fileChooser?.current?.click()}
+						>Choose image</button>
+					</label>
+					<input className="hidden" ref={fileChooser} id="image-select" type="file" onChange={handleFileChange}/>
+					<button 
+						className="bg-violet-800 text-white p-2 px-4"
+						onClick={handleCheckin} disabled={!file || isLoading}
+					>
+						{ isLoading ? 'Checking in...' : 'Check in' }
+					</button>
+				</div>
 			</div>
 		</>
 	)
